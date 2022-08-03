5ffa3dea83717c82f1fc10dfae83650f1849db7c
Scooterminsk <kickscooterby@gmail.com>
2022-08-03 12:56:39 +0300

    First experiments with Bezier lines

---
+++ Cards/BezierExperiments.playground/Contents.swift
@@ -0,0 +1,93 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+        self.view = view
+        
+        // curves creation on the scene
+        createBezier(on: view)
+    }
+    
+    private func createBezier(on view: UIView) {
+        // 1. grafic context (layer) creation, on which curves will be drown next
+        let shapeLayer = CAShapeLayer()
+        // 2. addition the layer as a child layer to the root layer of the root view
+        view.layer.addSublayer(shapeLayer)
+        // line color changing
+        shapeLayer.strokeColor = UIColor.gray.cgColor
+        // line width changing
+        shapeLayer.lineWidth = 5
+        // background color choosing
+        shapeLayer.fillColor = UIColor.green.cgColor
+        // shape creation
+        shapeLayer.path = getChiefsHat().cgPath
+    }
+    
+    private func getPath() -> UIBezierPath {
+        let path = UIBezierPath()
+        
+        // first triangle creation
+        path.move(to: CGPoint(x: 50, y: 50))
+        path.addLine(to: CGPoint(x: 150, y: 50))
+        path.addLine(to: CGPoint(x: 150, y: 150))
+        path.close()
+        
+        // second triangle creation
+        path.move(to: CGPoint(x: 50, y: 70))
+        path.addLine(to: CGPoint(x: 150, y: 170))
+        path.addLine(to: CGPoint(x: 50, y: 170))
+        path.close()
+        
+        return path
+    }
+    
+    private func getRect() -> UIBezierPath {
+        // rectangle entity creation
+        let rect = CGRect(x: 10, y: 10, width: 200, height: 100)
+        // rectangle creation
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.bottomRight, .topLeft], cornerRadii: CGSize(width: 30, height: 0))
+        return path
+    }
+    
+    private func getArc() -> UIBezierPath {
+        let centerPoint = CGPoint(x: 200, y: 200)
+        let path = UIBezierPath(arcCenter: centerPoint, radius: 150, startAngle: .pi/5, endAngle: .pi, clockwise: true)
+        return path
+    }
+    
+    private func getOval() -> UIBezierPath {
+        let rect = CGRect(x: 50, y: 50, width: 200, height: 100)
+        let path = UIBezierPath(ovalIn: rect)
+        return path
+    }
+    
+    private func getCurve() -> UIBezierPath {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 10, y: 10))
+        path.addCurve(to: CGPoint(x: 200, y: 200), controlPoint1: CGPoint(x: 200, y: 20), controlPoint2: CGPoint(x: 20, y: 200))
+        return path
+    }
+    
+    private func getChiefsHat() -> UIBezierPath {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 100, y: 100))
+        path.addArc(withCenter: CGPoint(x: 150, y: 100), radius: 50, startAngle: .pi, endAngle: 0, clockwise: true)
+        path.addLine(to: CGPoint(x: 220, y: 100))
+        path.addArc(withCenter: CGPoint(x: 220, y: 150), radius: 50, startAngle: .pi*3/2, endAngle: .pi/2, clockwise: true)
+        path.addLine(to: CGPoint(x: 200, y: 200))
+        path.addLine(to: CGPoint(x: 200, y: 260))
+        path.addLine(to: CGPoint(x: 100, y: 260))
+        path.addLine(to: CGPoint(x: 100, y: 200))
+        path.addLine(to: CGPoint(x: 80, y: 200))
+        path.addArc(withCenter: CGPoint(x: 80, y: 150), radius: 50, startAngle: .pi/2, endAngle: .pi*3/2, clockwise: true)
+        path.close()
+        return path
+    }
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()
