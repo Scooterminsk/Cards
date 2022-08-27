fef8b41a675f91e708a763d0ac184f59fb3ac2f3
Scooterminsk <kickscooterby@gmail.com>
2022-08-27 16:24:53 +0300

    4 new controllers for settings screen were added

---
+++ Cards/Controller/SettingScreenControllers/CardTypesController.swift
@@ -0,0 +1,29 @@
+//
+//  CardTypesController.swift
+//  Cards
+//
+//  Created by Zenya Kirilov on 27.08.22.
+//
+
+import UIKit
+
+class CardTypesController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
